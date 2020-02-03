ca95ead359b18f3384060b711d5ce68c7b4c0fdb
Liltar <60432201+Liltar@users.noreply.github.com>
2020-02-03 15:07:04 +0200

    Update 3.py

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -5,7 +5,8 @@ value = input('Enter the phrase to check is it a palindrome:')
 value1 = ''.join(reversed(value))
 if value == value1:
     print(value, 'is the palindrome')
-elif value is None:
     print('No input')
-elif:
+elif value is None:
+    pass
+else:
     print(value, 'is not the palindrome')
