5c1bd26877f5a44ee1af26a49fead4240f2f5c04
Liltar <60432201+Liltar@users.noreply.github.com>
2020-02-03 18:07:37 +0200

    Update 3.py

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -1,12 +1,10 @@
 # 3 palyndrome check
-# ERROR (неправильная интерпретация пробелов!!!
+# возможна неправильная интерпретация пробелов
 # решить проблему
 value = input('Enter the phrase to check is it a palindrome:')
 value1 = ''.join(reversed(value))
 if value == value1:
     print(value, 'is the palindrome')
     print('No input')
-elif value is None:
-    pass
 else:
     print(value, 'is not the palindrome')
