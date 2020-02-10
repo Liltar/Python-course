44a033e55567055721452c7ed6fb14a3a532be6c
Liltar <60432201+Liltar@users.noreply.github.com>
2020-02-10 15:16:25 +0200

    Update

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -1,8 +1,6 @@
 # 6 Дана строка "English = 78 Science = 83 Math = 68 History = 65".
 # Вычислить сумму всех чисел в строке.
 string1 = 'English = 78 Science = 83 Math = 68 History = 65'
-
-#my_set = set(string1)
-#print(my_set)
-# print(sum_digits(""))
-# def sum_digits(digit):
+def sum_digits(digit):
+    return sum(int(x) for x in digit if x.isdigit())
+print(sum_digits(string1))
