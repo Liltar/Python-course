09bfb1c6a33fed0db8a84341b54148482f00fff9
Liltar <60432201+Liltar@users.noreply.github.com>
2020-02-27 16:41:08 +0200

    UPD

--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,4 +1,5 @@
 # Сортировать список от меньшего к большему с помощью heapq
-l = [['Krishna', 67, 68, 69], ['Arjun', 70, 98, 63], ['Malika', 52, 56, 60]]
 import heapq
-for
+l = [['Krishna', 67, 68, 69], ['Arjun', 70, 98, 63], ['Malika', 52, 56, 60]]
+heapq.heapify(l)
+print([heapq.heappop(l), heapq.heappop(l), heapq.heappop(l)])
