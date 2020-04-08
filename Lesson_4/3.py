04c4e6a47557efd84015acaa00adb72413750110
Liltar <60432201+Liltar@users.noreply.github.com>
2020-04-08 17:28:26 +0300

    Add files via upload

--- Lesson_4/3.py
+++ Lesson_4/3.py
@@ -2,3 +2,25 @@
 # (1) длина больше 4 символов,
 # (2) содержит только маленькие латинские буквы и цифры,
 # (3) число букв должно быть нечетным, а цифр четным.
+password = input('Enter a password:')
+# 1 длина больше 4 символов
+if len(password) > 4:
+    print('password is ok')
+else:
+    raise Exception('Length of password should be more than 4')
+
+# The length was:
+
+# 2 содержит только маленькие латинские буквы и цифры
+# import string
+# string.ascii_lowercase
+condition_1 = list('abcdefghijklmnopqrstuvwxyz')
+condition_2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for i in password:
+
+    print('Good')
+else:
+    raise Exception('Wrong symbol')
+# if i in password:
+#    i = condition_1 and i = condition_2
+#    print('password is ok')
